49e6aa0d0a6b7872337507e7767ca51f766b6373
Carlos Henrique Alvarenga dos Santos <carlosho200@hotmail.com>
2022-01-23 11:04:09 -0400

    Add dependencies: eslint
    Configure eslint

---
+++ .eslintrc.js
@@ -0,0 +1,53 @@
+module.exports = {   
+    root: true, 
+    env: {
+        node: true,
+        es2021: true,
+        jest: true
+    },
+    extends: "eslint:recommended",
+    parserOptions: {
+        ecmaVersion: "latest"
+    },
+    rules: {                
+        //Suggestions
+        "capitalized-comments": "error",
+        "camelcase": "error",
+        "curly": "error",        
+        "eqeqeq": "error",
+        "max-classes-per-file": ["error", 1],    
+        "max-nested-callbacks": ["error", 4],
+        "max-params": ["error", {"max": 4}],
+        "new-cap": ["error", {"newIsCap": true}],
+        "strict": ["error", "global"],
+        //Layout and Format
+        "array-bracket-spacing": "warn",
+        "arrow-parens": "warn",
+        "arrow-spacing": ["warn", {
+            "before": true, 
+            "after": true
+        }],
+        "brace-style": "warn",
+        "comma-dangle": "warn",
+        "comma-spacing": "warn",
+        "indent": [
+            "error", 
+            4,
+            {"SwitchCase": 1}
+        ],         
+        "key-spacing": ["warn", {
+            "beforeColon": false, 
+            "afterColon": true
+        }],
+        "lines-between-class-members": ["warn"],
+        "max-len": ["warn", {"code": 100, "comments": 80}],
+        "max-statements-per-line": ["warn", {"max": 1}],
+        "new-parens": "error",
+        "newline-per-chained-call": "warn",
+        "no-trailing-spaces": "warn",
+        "no-whitespace-before-property": "warn",
+        "quotes": ["error", "double"],
+        "semi": ["error", "always"],
+        
+    }
+};
